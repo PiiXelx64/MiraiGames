092e8a512bd134d4b6f89d0794f2093e11101a68
benoit_arnlt <benar77@gmail.com>
2020-03-28 15:37:40 +0100

    added nodemon

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 8080;
-
 
 const winver = "path to winver";
 const linuxver = "path to linuxver";
@@ -9,16 +9,17 @@ const osxver = "path to osxver";
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile('index.html'));
-app.get('/fr/a-propos', (req, res) => res.sendFile("fr/about_us.html"));
-app.get('/fr/media', (req, res) => res.sendFile("fr/medias.html"));
-app.get('/fr/telecharger', (req,  res) => res.sendFile('fr/download.html'))
+app.get('/', (req, res) => res.sendFile('index.html', {root: path.join(__dirname, 'public/')}));
+app.get('/fr/a-propos', (req, res) => res.sendFile("about_us.html", {root: path.join(__dirname, 'public/fr')}));
+app.get('/fr/media', (req, res) => res.sendFile("/medias.html", {root: path.join(__dirname, 'public/fr')}));
+app.get('/fr/telecharger', (req,  res) => res.sendFile('download.html', {root: path.join(__dirname, 'public/fr')}))
 
-app.get('/en/', (req, res) => res.sendFile('en/index.html'));
-app.get('/en/about', (req, res) => res.sendFile('en/about_us.html'))
-app.get('/en/media', (req, res) => res.sendFile('en/medias.html'))
+app.get('/en/', (req, res) => res.sendFile('en/index.html', {root: path.join(__dirname, 'public/en')}));
+app.get('/en/about', (req, res) => res.sendFile('about_us.html', {root: path.join(__dirname, 'public/en')}))
+app.get('/en/media', (req, res) => res.sendFile('medias.html', {root: path.join(__dirname, 'public/en')}))
+app.get('/en/download', (req, res) => res.sendFile('download.html', {root: path.join(__dirname, 'public/en')}))
 
-
+app.use((req, res, next) => res.status(404).sendFile('error_404.html', {root: path.join(__dirname, 'public/en')}));
 
 function download(version) {
     if(typeof version === "string") {
