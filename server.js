e9f5a88fc927a3f0d624acad23c4f4b79cfc4475
benoit_arnlt <benar77@gmail.com>
2020-04-02 11:16:50 +0200

    updated links

--- server.js
+++ server.js
@@ -3,21 +3,21 @@ const path = require('path');
 const app = express();
 const port = 8080;
 
-const winver = "path to winver";
-const linuxver = "path to linuxver";
-const osxver = "path to osxver";
+const winver = "TWWIN.zip";
+const linuxver = "TWOSX.zip";
+const osxver = "TWLINUX.zip";
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile('index.html', {root: path.join(__dirname, 'public/')}));
 app.get('/fr/a-propos', (req, res) => res.sendFile("about_us.html", {root: path.join(__dirname, 'public/fr')}));
 app.get('/fr/media', (req, res) => res.sendFile("/medias.html", {root: path.join(__dirname, 'public/fr')}));
-app.get('/fr/telecharger', (req,  res) => res.sendFile('download.html', {root: path.join(__dirname, 'public/fr')}))
+app.get('/fr/telecharger', (req,  res) => res.sendFile('fr/telecharger', {root: path.join(__dirname, 'public/fr')}))
 
 app.get('/en/', (req, res) => res.sendFile('en/index.html', {root: path.join(__dirname, 'public/en')}));
 app.get('/en/about', (req, res) => res.sendFile('about_us.html', {root: path.join(__dirname, 'public/en')}))
 app.get('/en/media', (req, res) => res.sendFile('medias.html', {root: path.join(__dirname, 'public/en')}))
-app.get('/en/download', (req, res) => res.sendFile('download.html', {root: path.join(__dirname, 'public/en')}))
+app.get('/en/download', (req, res) => res.sendFile('fr/telecharger', {root: path.join(__dirname, 'public/en')}))
 
 app.use((req, res, next) => res.status(404).sendFile('error_404.html', {root: path.join(__dirname, 'public/en')}));
 
@@ -29,7 +29,7 @@ function download(version) {
                 break;
             case "LINUX":
                 return linuxver;
-            case "OSXVER":
+            case "OSX":
                 return osxver;
                 break;
             default: 
